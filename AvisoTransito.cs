a26e50204f19dae8b2a85285d3ee28c07954e500
DermisZabala <dermiszabala@gmail.com>
2024-06-19 00:51:43 -0400

    Ejemplo utilizando interface

---
+++ AvisoTransito.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_3
+{
+    internal class AvisoTransito: IAviso
+    {
+        private string fecha;
+        private string remitente;
+        private string mensaje;
+
+        public AvisoTransito()
+        {
+            remitente = "DIGESETT";
+            mensaje = "Sanción cometida. Pague antes de 3 días y se le hara una reducción al pago de un 50%.";
+            fecha = "18/06/2024";
+        }
+        public AvisoTransito(string remitente, string mensaje, string fecha)
+        {
+            this.remitente = remitente;
+            this.mensaje = mensaje;
+            this.fecha = fecha;
+        }
+        public void MostrarAviso()
+        {
+            Console.WriteLine("Remitente: {0} \nMensaje: \n{1} \nFecha: {2}", remitente, mensaje, fecha);
+        }
+        public string Fecha()
+        {
+            return fecha;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Interface_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AvisoTransito aviso1 = new AvisoTransito();
+            aviso1.MostrarAviso();
+
+            AvisoTransito aviso2 = new AvisoTransito("DIGESETT", "Sanción de velocidad RD1500", "22/09/2004");
+            aviso2 .MostrarAviso();
+        }
+    }
+}
